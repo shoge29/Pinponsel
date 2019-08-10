4a32be597b9b13b303a7f93ab22e46d5d97ccd74
shoge29 <53959352+shoge29@users.noreply.github.com>
2019-08-10 13:09:36 +0700

    Add files via upload

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://kompisafelinkv2.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="extending-battery-life-of-mobile.html"}
+if(x=="2"){xxx="what-is-5g-and-how-it-will-evolve.html"}
+if(x=="3"){xxx="developing-mobile-apps-can-drive-revenue.html"}
+if(x=="4"){xxx="cloud-computing-predictions.html"}
+if(x=="5"){xxx="advantages-cloud-based-it-infrastructures.html"}a_to_ve[i].href="https://fathancellular.blogspot.com/2019/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
